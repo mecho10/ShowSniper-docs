5dcc65e1377829c783d6d70bfc193d5c713a282c
Leroy <leroy@Macbook-pro.local>
2025-07-26 03:25:53 +0800

    Deploy static website

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/leroy/ShowSniper-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/leroy/ShowSniper-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/leroy/ShowSniper-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/leroy/ShowSniper-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/leroy/ShowSniper-docs/src/css/custom.css"),
-];
